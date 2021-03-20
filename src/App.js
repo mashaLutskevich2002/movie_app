22faff5bf3adb70a464ed473e4c6857cb431365c
mashaLutskevich2002 <57455346+mashaLutskevich2002@users.noreply.github.com>
2021-03-20 14:46:13 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import icon from '../public/img/'
+import  icon from './img/icon-arrow-down.svg';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MainDiv />
+  );
+}
+
+
+const MainDiv = () => {
+  return (
+    <div className='main'>
+      <LeftDiv />
+      <img className='svgThird' src='../img/illustration-box-desktop.svg' alt='box'></img>
+      <RightDiv />
     </div>
   );
 }
+
+
+const LeftDiv = () => {
+  return (
+    <div className='main__pic'>
+      <svg
+        className='svgFirst'
+        width="100%"
+        height='100%'
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g fillRule="nonzero" fill="none" opacity=".099">
+          <path d="M468.755 937.353L19.744 671.73c-14.295-8.047-26.548-21.23-12.62-29.878L457.734 381.89c6.754-4.194 17.018-6.946 23.903-2.936l471.265 273.827c14.296 8.308 16.861 31.45 2.619 39.837L527.925 936.933a59.64 59.64 0 01-59.17.42z" fill="#6862E6" />
+          <path d="M468.755 561.353L19.744 295.73c-14.295-8.047-26.548-21.23-12.62-29.878L457.734 5.89c6.754-4.194 17.018-6.946 23.903-2.936l471.265 273.827c14.296 8.308 16.861 31.45 2.619 39.837L527.925 560.933a59.64 59.64 0 01-59.17.42z" stroke="#1E1F36" />
+          <path d="M468.755 636.353L19.744 370.73c-14.295-8.047-26.548-21.23-12.62-29.878L457.734 80.89c6.754-4.194 17.018-6.946 23.903-2.936l471.265 273.827c14.296 8.308 16.861 31.45 2.619 39.837L527.925 635.933a59.64 59.64 0 01-59.17.42z" stroke="#1E1F36" />
+        </g>
+      </svg>
+      <img className='svgSecond' src='../img/illustration-woman-online-desktop.svg' alt='women'></img>
+      <img className='svgForth' src='../img/bg-pattern-mobile.svg' alt='box'></img>
+    </div>
+  );
+}
+
+
+const RightDiv = () => {
+  return (
+    <div>
+      <Zagolovok />
+      <Accardion />
+    </div>
+
+  );
+}
+
+
+const Zagolovok = () => {
+  return (
+    <div className='FAQ'>
+      <h1>FAQ</h1>
+    </div>
+  );
+}
+
+
+const Accardion = () => {
+  const questions = {
+    first: {
+      question: 'How many team members can I invite?',
+      answer: 'You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.'
+    },
+    second: {
+      question: 'What is the maximum file upload size?',
+      answer: 'No more than 2GB. All files in your account must fit your allotted storage space.'
+    },
+    third: {
+      question: 'How do I reset my password?',
+      answer: 'Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.'
+    },
+    forth: {
+      question: 'Can I cancel my subscription?',
+      answer: 'Yes! Send us a message and we’ll process your request no questions asked.'
+    },
+
+    fifth: {
+      question: ' Do you provide additional support? ',
+      answer: 'Chat and email support is available 24/7. Phone lines are open during normal business hours.'
+    }
+  }
+
+  const listItems = Object.keys(questions).map(
+    (key) => {
+      return (
+        <AccordionItem id={key} obj={questions[key]} />
+      )
+    }
+
+  );
+
+  return (
+    <div>
+      { listItems}
+    </div>
+
+  );
+}
+
+
+const AccordionItem = (props) => {
+  return (
+    <div>
+      <div>
+        <Question questionId={props.id} text={props.obj.question} imgId={props.id} />
+        <Answer answerId={props.id} text={props.obj.answer} />
+      </div>
+      <hr></hr>
+    </div >
+  )
+}
+
+const clickEventhandler = (e, answer) => {
+  e.preventDefault();
+  const a = document.getElementById(answer);
+  const strelka = e.target.childNodes[1];
+  console.log('==== Arrow');
+  console.log(strelka)
+  if (a.style.display === 'none') {
+    a.style.display = 'block';
+    strelka.className = 'svgStrelka another';
+  } else {
+    a.style.display = 'none';
+    strelka.className = 'svgStrelka';
+
+  }
+}
+
+
+const Question = (props) => {
+  return (
+    <div className='quest' onClick={(e) => clickEventhandler(e, props.questionId)}>
+      <p>{props.text}</p>
+      <img id={props.imgId} src={icon} alt='strelka' />
+    </div>
+  )
+
+}
+
+
+const Answer = (props) => {
+  return (
+    <div className='answer' id={props.answerId}>
+      <p>{props.text}</p>
+    </div>
+  )
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.getElementById('root'));
+ // const Answer1 = () =>{
+  //   return(
+  //        <div className='answer' id='answer1'>
+  //       <p> You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan. </p>
+  //     </div>
+
+  //   )
+  // }
+  // const Answer2 = () =>{
+  //   return(
+  //    <div className='answer' id='answer2'>
+  //       <p>  No more than 2GB. All files in your account must fit your allotted storage space. </p>
+  //   </div>
+
+  //   )
+  // }
+  // const Answer3 = () =>{
+  //   return(
+  //    <div className='answer' id='answer3'>
+  //       <p>  Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you. </p>
+  //     </div>
+
+  //   )
+  // }
+  // const Answer4 = () =>{
+  //   return(
+  //    <div className='answer' id='answer4'>
+  //       <p> Yes! Send us a message and we’ll process your request no questions asked. </p>
+  //     </div>
+
+  //   )
+  // }
+  // const Answer5 = () =>{
+  //   return(
+  //   <div className='answer' id='answer5'>
+  //     <p> Chat and email support is available 24/7. Phone lines are open during normal business hours. </p>
+  //   </div>
+  //   )
+  // }
+
+
